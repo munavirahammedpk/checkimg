1fd6de99c704b5b0197f0d1e1bbcce7f336d8c3f
MoneyManagerApp <munavirofficial786@gmail.com>
2022-09-04 22:08:22 +0530

    api 2 added

--- api/product.js
+++ api/product.js
@@ -13,4 +13,16 @@ router.get('/',async(req,res)=>{
     }
 });
 
+router.get('/api2',async(req,res)=>{
+    try {
+        res.json({
+            status:200,
+            message:'get data has successfully 2',
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('server error')
+    }
+});
+
 module.exports=router;
